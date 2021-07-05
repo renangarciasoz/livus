3d026ee520fb9ad01169774ca648e1aa683f9e1d
renangarciasoz <renangarciasoz@hotmail.com>
2021-07-05 01:31:29 -0300

    chore: change font family

--- src/configs/theme.ts
+++ src/configs/theme.ts
@@ -2,6 +2,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+  },
   palette: {
     primary: {
       main: '#1A1A1A',
