ec67d06d20c2440496dd32fb804424c49dc1a547
renangarciasoz <renangarciasoz@hotmail.com>
2021-07-07 00:58:31 -0300

    feat: finish subscribers page and start classes page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,19 @@ import { CreatorPageLayout } from 'layouts/creator-page';
 import { CreatorProfile, Subscribers, Navigation } from 'modules/creator';
 import { Box } from '@material-ui/core';
 import React from 'react';
+import { NextPageContext } from 'next';
+import { URL_SUBSCRIBERS } from 'constants/urls';
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const { page } = ctx.query;
+
+  if (!page && ctx.res) {
+    ctx.res.setHeader('location', `${URL_SUBSCRIBERS}/1`);
+    ctx.res.statusCode = 302;
+    ctx.res.end();
+    return { props: {} };
+  }
+}
 
 export default function Home(): React.ReactElement {
   return (
